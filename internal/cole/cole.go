0b3014dd04c0c10d7ddafe53fcb43a9dd1058f86
Nicolas Takashi <nicolas.takashi@farfetch.com>
2021-12-08 00:17:45 +0000

    [REFACTORY] change logging

--- internal/cole/cole.go
+++ internal/cole/cole.go
@@ -18,11 +18,12 @@ func Start(ctx context.Context, scmd command.Server) error {
 	}
 	t := time.NewTimer(1 * time.Millisecond)
 	lastSinceTime := time.Now().Add(time.Duration(-24) * time.Hour)
+	logHandler := loghandler.New()
 
 	for {
 		select {
 		case <-t.C:
-			if err := start(ctx, scmd, client, &lastSinceTime); err != nil {
+			if err := start(ctx, scmd, client, &lastSinceTime, logHandler); err != nil {
 				// syncErrorTotal.Inc()
 				return err
 			} else {
@@ -38,7 +39,7 @@ func Start(ctx context.Context, scmd command.Server) error {
 	}
 }
 
-func start(ctx context.Context, scmd command.Server, client *k8sclient.K8sClient, lastSinceTime *time.Time) error {
+func start(ctx context.Context, scmd command.Server, client *k8sclient.K8sClient, lastSinceTime *time.Time, lh loghandler.LogHandler) error {
 	pods, err := client.ListPods(ctx, scmd.Namespace, scmd.LabelSelector)
 
 	if err != nil {
@@ -49,17 +50,17 @@ func start(ctx context.Context, scmd command.Server, client *k8sclient.K8sClient
 	logs := []k8sclient.LogLine{}
 
 	for _, pod := range pods {
-		logs, err = client.GetPodLogs(ctx, scmd.Namespace, pod, *lastSinceTime)
+		lgs, err := client.GetPodLogs(ctx, scmd.Namespace, pod, *lastSinceTime)
 		if err != nil {
 			logrus.Errorf("error to get pod %v logs ", pod.Name, err)
 			return err
 		}
+		logs = append(logs, lgs...)
 	}
 
 	*lastSinceTime = time.Now()
-	logHandler := loghandler.New()
 	for _, log := range logs {
-		logHandler.Handle(log)
+		lh.Handle(log)
 	}
 
 	return nil
